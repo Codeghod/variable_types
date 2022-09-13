ddbf851add619fb8279f992c07eae81281447c71
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-13 16:57:46 -0700

    Add files via upload

---
+++ python data types.py
@@ -0,0 +1,24 @@
+# python data types
+# Float data type
+
+a = 10.7
+b = 3.3
+print(a + b)
+
+# Int data type
+
+c = 10
+d = 3
+print(c + d)
+
+# String data type
+
+e = 'Alfred'
+f = 'Turkson'
+print(e)
+print(f)
+print(e + f)
+
+# Boolean data type
+
+print('with the boolean data type it deals with the true or false options')
